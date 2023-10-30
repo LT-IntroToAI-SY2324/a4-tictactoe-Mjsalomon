f99a4dab430abfb705cff41087807db17a304610
Martin Salomon <mjsalomon@cps.edu>
2023-10-30 10:02:07 -0500

    notes

--- a4.py
+++ a4.py
@@ -2,6 +2,9 @@
 # talking about unimplemented class attributes, don't worry about this as you're working
 
 
+from operator import truediv
+
+
 class TTTBoard:
     """A tic tac toe board
 
@@ -9,8 +12,71 @@ class TTTBoard:
         board - a list of '*'s, 'X's & 'O's. 'X's represent moves by player 'X', 'O's
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
+    def __init__(self):
+        self.board = ['*'] * 9
 
-    pass
+    def __str__(self):
+        s = ""
+        for x in [0, 3, 6]:
+            s += self.board[x + 0] + " " + self.board[x + 1] + " " + self.board[x + 2] + "\n"
+        return s
+
+    def make_move(self, player, pos) -> bool:
+        # fail if invalid move
+        if pos < 0 or pos > 8 or self.board[pos] != '*':
+            return False
+
+        # otherwise make the move
+        self.board[pos] = player
+        return True
+
+    def has_won(self, player) -> bool:
+        check = player + player + player
+        
+        # Checks horizontal wins
+        for x in [0, 3, 6]:
+            s = ""
+            s += self.board[x + 0] + self.board[x + 1] + self.board[x + 2]
+
+            if check == s:
+                return True
+
+        # Checks vertical wins
+        for x in [0, 1, 2]:
+            s = ""
+            s += self.board[x + 0] + self.board[x + 3] + self.board[x + 6]
+
+            if check == s:
+                return True
+
+        # Checks both diagonal wins
+        s = self.board[0] + self.board[4] + self.board[8]
+        if s == check:
+            return True
+
+        s = self.board[3] + self.board[4] + self.board[6]
+        if s == check:
+            return True
+        
+        return False
+
+
+    def game_over(self):
+        temp = False
+        for x in range(9):
+            if self.board[x] == "*":
+                break
+            else:
+                temp == True
+
+        if self.has_won("X") == True or self.has_won("O") == True or x == True:
+            return True
+
+        return False
+
+
+    def clear(self):
+        self.board = ["*"] * 9
 
 
 def play_tic_tac_toe() -> None:
